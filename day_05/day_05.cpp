265dc92d7ab1f79633ebb4e8a345822fbcf8ff4c
Juan Ruiz <juan.ruiz@parrot.com>
2023-12-06 15:36:57 +0100

    [DEV] add day5 part 2

--- day_05/day_05.cpp
+++ day_05/day_05.cpp
@@ -3,11 +3,12 @@
 #include <sstream>
 #include <string>
 #include <vector>
+#include <set>
 
 using namespace std;
 
-#define USE_SAMPLE
-// #define IS_SECOND_PART
+// #define USE_SAMPLE
+#define IS_SECOND_PART
 
 #define CHECK_FILE_OPEN(FS, FN)\
 	ifstream FS(FN);\
@@ -84,14 +85,21 @@ class RangeHolder {
 			cout << endl;
 		}
 
-		uint64_t find_key(uint64_t key) {
+		uint64_t find_key(uint64_t key, uint64_t *valid_range) {
 			for (Range r: map) {
 				uint64_t to_key = r.from_key + r.length-1;
 				if (key >= r.from_key && key <= to_key) {
+
+					uint64_t dist_key_end = to_key - key;
+					if (dist_key_end < *valid_range)
+						*valid_range = dist_key_end;
+
 					uint64_t dist_key_key = key - r.from_key;
 					return r.from_val + dist_key_key;
 				}
 			}
+
+			*valid_range = 0;	
 			return key;
 		}
 };
@@ -113,15 +121,17 @@ RangeHolder humidity;
 RangeHolder location;
 
 
-uint64_t get_location (uint64_t id)
+uint64_t get_location (uint64_t id, uint64_t *valid_range)
 {
-	uint64_t ksoil = soil.find_key(id);
-	uint64_t kfertilizer = fertilizer.find_key(ksoil);
-	uint64_t kwater = water.find_key(kfertilizer);
-	uint64_t klight = light.find_key(kwater);
-	uint64_t ktemperature = temperature.find_key(klight);
-	uint64_t khumidity = humidity.find_key(ktemperature);
-	uint64_t klocation = location.find_key(khumidity);
+	*valid_range = 0;
+	uint64_t dummy;
+	uint64_t ksoil = soil.find_key(id, valid_range);
+	uint64_t kfertilizer = fertilizer.find_key(ksoil, &dummy);
+	uint64_t kwater = water.find_key(kfertilizer,  &dummy);
+	uint64_t klight = light.find_key(kwater,  &dummy);
+	uint64_t ktemperature = temperature.find_key(klight,  &dummy);
+	uint64_t khumidity = humidity.find_key(ktemperature,  &dummy);
+	uint64_t klocation = location.find_key(khumidity,  &dummy);
 
 	return klocation;
 }
@@ -217,17 +227,18 @@ int main (void)
 	water.print_content();
 
 	uint64_t min_location = -1;
+
 #ifdef IS_SECOND_PART
-	for (int i=0; i<seeds.size()-1; i+=2) {
+	for (uint64_t i=0; i<seeds.size()-1; i+=2) {
 		Seed s = seeds.at(i);
 		Seed s2 = seeds.at(i+1);
 
 		uint64_t to_seed = s.id + s2.id - 1;
-
 		cout << "DOING SEED: " << s.id << " TO SEED: " << to_seed << endl;
 
-		for(int j=s.id; j<=to_seed; j++){
-			uint64_t location = get_location(j);
+		uint64_t valid_range = 0;
+		for(uint64_t j=s.id; j<=to_seed; j = j + 1 + valid_range){
+			uint64_t location = get_location(j, &valid_range);
 			if (location < min_location)
 				min_location = location;
 		}
@@ -235,6 +246,112 @@ int main (void)
 	}
 
 	cout << "The response part two is: " << min_location << endl;
+
+	/* Optimize soils */
+	// set<uint64_t> op_soils;
+	// set<uint64_t> op_fertilizer;
+	// set<uint64_t> op_water;
+	// set<uint64_t> op_light;
+	// set<uint64_t> op_temperature;
+	// set<uint64_t> op_humidity;
+	// set<uint64_t> op_location;
+	// uint64_t valid_range = 0;
+	// uint64_t current_th = 0;
+
+	// for (uint64_t i=0; i<seeds.size()-1; i+=2) {
+	// 	Seed s = seeds.at(i);
+	// 	Seed s2 = seeds.at(i+1);
+
+	// 	uint64_t from_seed = s.id; 
+	// 	uint64_t to_seed = s.id + s2.id - 1;
+	// 	cout << "DOING SEED: " << from_seed << " TO SEED: " << to_seed << endl;
+
+	// 	for(uint64_t j=from_seed; j<=to_seed; j = j + 1 + valid_range) {
+	// 		uint64_t ksoil = soil.find_key(j, &valid_range);
+			// This doesnt work as it takes too much memory
+	// 		op_soils.insert(ksoil);
+	// 	}
+	// }
+
+	/*
+	* Order:
+	* seeds -> soil -> fertilizer -> water -> light
+	* light -> temperature -> humidity -> location
+	* 
+	* 178159714 TOO HIGH
+	*/
+
+	// valid_range = 0;
+	// current_th = 0;
+	// for (uint64_t v : op_soils) {
+	// 	if (v <= current_th)
+	// 		continue;
+
+	// 	uint64_t kfertilizer = fertilizer.find_key(v, &valid_range);
+	// 	op_fertilizer.insert(kfertilizer);
+	// 	current_th = v + valid_range;
+	// }
+
+	// valid_range = 0;
+	// current_th = 0;
+	// for (uint64_t v : op_fertilizer) {
+	// 	if (v <= current_th)
+	// 		continue;
+
+	// 	uint64_t kwater = water.find_key(v, &valid_range);
+	// 	op_water.insert(kwater);
+	// 	current_th = v + valid_range;
+	// }
+
+	// valid_range = 0;
+	// current_th = 0;
+	// for (uint64_t v : op_water) {
+	// 	if (v <= current_th)
+	// 		continue;
+
+	// 	uint64_t klight = light.find_key(v, &valid_range);
+	// 	op_light.insert(klight);
+	// 	current_th = v + valid_range;
+	// }
+
+	// valid_range = 0;
+	// current_th = 0;
+	// for (uint64_t v : op_light) {
+	// 	if (v <= current_th)
+	// 		continue;
+
+	// 	uint64_t ktemperature = temperature.find_key(v, &valid_range);
+	// 	op_temperature.insert(ktemperature);
+	// 	current_th = v + valid_range;
+	// }
+
+	// valid_range = 0;
+	// current_th = 0;
+	// for (uint64_t v : op_temperature) {
+	// 	if (v <= current_th)
+	// 		continue;
+
+	// 	uint64_t khumidity = humidity.find_key(v, &valid_range);
+	// 	op_humidity.insert(khumidity);
+	// 	current_th = v + valid_range;
+	// }
+
+	// valid_range = 0;
+	// current_th = 0;
+	// for (uint64_t v : op_humidity) {
+	// 	if (v <= current_th)
+	// 		continue;
+
+	// 	uint64_t klocation = location.find_key(v, &valid_range);
+	// 	op_location.insert(klocation);
+	// 	current_th = v + valid_range;
+	// }
+
+	// for (uint64_t v : op_location) {
+	// 	cout << "LOCATION: " << v << endl;
+	// }
+
+	// cout << "The response part two is: " << *op_location.begin() << endl;
 
 #else
 	for (int i=0; i<seeds.size(); i+=1) {
@@ -244,9 +361,11 @@ int main (void)
 		if (location < min_location)
 			min_location = location;
 	}
-#endif
 
 	cout << "The response part one is: " << min_location << endl;
+#endif
+
+	
 
 	return 0;
 }
