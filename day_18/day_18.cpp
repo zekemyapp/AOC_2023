ac8f9071652127a73d6f0579872babddb6d4ae0c
Juan Ruiz <juan.ruiz@parrot.com>
2023-12-19 08:50:22 +0100

    [DEV] add day18

---
+++ day_18/day_18.cpp
@@ -0,0 +1,105 @@
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <algorithm>
+#include <vector>
+#include <tuple>
+#include <set>
+#include <unordered_set>
+
+#include <stdlib.h>
+
+typedef std::tuple<int, int> Position;
+
+class Line {
+	public:
+		Position start;
+		Position end;
+		std::string color;
+
+		Line(Position s, Position e, std::string c) : start(s), end(e), color(c) {}
+};
+
+int64_t matrix_solve (Position a, Position b)
+{
+	int x1, x2, y1, y2;
+	std::tie(y1, x1) = a;
+	std::tie(y2, x2) = b;
+
+	return (x1 * y2) - (x2 * y1);
+}
+
+int main (void) {
+	std::cout << "Starting day 18" << std::endl;
+
+	std::string filename = "sample.txt";
+	std::ifstream input_file(filename);
+	int64_t result_one = 0;
+	int64_t result_two = 0;
+
+	std::vector<std::string> field;
+	std::vector<Line> lines;
+	Position last_pos = Position(0, 0);
+	Position pos;
+	int i,j;
+	int boundaries = 0;
+
+	/* Check if input file was correctly opened */
+	if (!input_file.is_open()) {
+		std::cout << "Had problems opening file " << filename << std::endl;
+		return -1;
+	}
+
+	/* Read input into file */
+	while (!input_file.eof()) {
+		std::string file_content;
+		getline(input_file, file_content);
+
+		char dir = file_content.at(0);
+		size_t div1 = file_content.find_first_of(' ');
+		size_t div2 = file_content.find_first_of(' ', div1+1);
+		std::string sholes = file_content.substr(div1+1, div2-(div1+1));
+		int nholes = atoi(sholes.c_str());
+		div1 = file_content.find_first_of("#");
+		std::string hcolor = file_content.substr(div1, 7);
+
+		std::tie(i,j) = last_pos;
+		int n_i = i, n_j = j;
+		switch (dir) {
+			case 'U':
+				n_i = i - nholes;
+				break;
+			case 'R':
+				n_j = j + nholes;
+				break;
+			case 'D':
+				n_i = i + nholes;
+				break;
+			case 'L':
+				n_j = j - nholes;
+				break;
+		}
+
+		boundaries += nholes;
+
+		pos = Position(n_i, n_j);
+		lines.push_back(Line(last_pos, pos, hcolor));
+		last_pos = pos;
+	}
+
+	int64_t a2 = 0;
+	for (auto l : lines) {
+		a2 += matrix_solve(l.start, l.end);
+	}
+
+	int64_t area = a2 / 2;
+	int64_t internal = area + 1 - (boundaries/2);
+	result_one = internal + boundaries;
+
+	std::cout << "area " << area << std::endl;
+	std::cout << "boundaries " << boundaries << std::endl;
+	std::cout << "internal " << internal << std::endl;
+
+	std::cout << "the result one is " << result_one << std::endl;
+	return 0;
+}
